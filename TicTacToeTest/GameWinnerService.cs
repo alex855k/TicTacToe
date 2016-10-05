984dc870616e49e514040fea6ca32583fee40065
alex855k <alexander2341@gmail.com>
2016-10-05 08:26:11 +0200

    Add project files.

---
+++ TicTacToeTest/GameWinnerService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeTest
+{
+    class GameWinnerService : IGameWinnerService
+    {
+
+        public char[,] gameBoard = new char[3, 3] { {' ', ' ', ' ' },
+                                                 {' ', ' ', ' ' },
+                                                 {' ', ' ', ' ' } };
+
+
+        public char CheckForThreeInARowHorizontalRow(char[,] gameBoard) {
+
+            var columnOneChar = gameBoard[0, 0];
+            var columnTwoChar = gameBoard[0, 1];
+            var columnThreeChar = gameBoard[0, 2];
+            if (columnOneChar == columnTwoChar &&
+                columnTwoChar == columnThreeChar)
+            {
+                return columnOneChar;
+            }
+            return ' ';
+        }
+
+        public char Validate(char[,] board)
+        {
+
+            var currentWinningSymbol = ' ';
+           
+            currentWinningSymbol = this.CheckForThreeInARowHorizontalRow(board);
+      
+            if(currentWinningSymbol != ' ') {
+                return currentWinningSymbol;
+            }
+            currentWinningSymbol = this.CheckForThreeInARowVerticalColumn(board);
+            return currentWinningSymbol;
+        }
+
+        private char CheckForThreeInARowVerticalColumn(char[,] board)
+        {
+            var rowOneChar = board[0, 0];
+            var rowTwoChar = board[1, 0];
+            var rowThreeChar = board[2, 0];
+            if (rowOneChar == rowTwoChar &&
+                rowTwoChar == rowThreeChar)
+            {
+                return rowOneChar;
+            }
+            return ' ';
+        }
+    }
+}
---
+++ TicTacToeTest/Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToeTest
+{
+    [TestClass]
+    public class Test
+    {
+
+        GameWinnerService _gameWinnerService = new GameWinnerService();
+    
+
+        [TestMethod]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] { {' ', ' ', ' ' },
+                                        {' ', ' ', ' ' },
+                                        {' ', ' ', ' ' } };
+
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void PlayerWithAllSpacesInTopRowIsWinner() {
+           
+            const char expected = 'X';
+            var gameBoard = new char[3, 3] { { expected, expected, expected},
+                                             {' ', ' ', ' '},
+                                             {' ', ' ', ' '} };
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void PlayerWithAllSpacesInFirstColumnIsWinner()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3] { { expected, expected, expected},
+                                             {' ', ' ', ' '},
+                                             {' ', ' ', ' '} };
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                gameBoard[columnIndex, 0] = expected;
+            }
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
